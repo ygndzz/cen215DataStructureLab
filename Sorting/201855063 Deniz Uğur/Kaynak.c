10b2bda6fb1a8f7bb835163cc6e9bdddcae3a379
denizugur99 <73538275+denizugur99@users.noreply.github.com>
2020-11-01 22:19:58 +0300

    Update Kaynak.c
    heapsort ve quicksort eklendi

--- Sorting/201855063 Deniz Uğur/Kaynak.c
+++ Sorting/201855063 Deniz Uğur/Kaynak.c
@@ -2,6 +2,125 @@
 #include<time.h>
 #include<conio.h>
 clock_t start_t, end_t, total_t;
+void swap(int* a, int* b) {
+	int temp;
+	temp = *a;
+	*a = *b;
+	*b = temp;
+
+}
+
+void quicksort(int arr[], int first, int last) {
+	int i, j, pivot, temp;
+	if (first < last) {
+		pivot = first;
+		i = first;
+		j = last;
+		while (i < j) {
+			while (arr[i] <= arr[pivot] && i < last)
+				i++;
+			while (arr[j] > arr[pivot])
+				j--;
+			if (i < j) {
+				swap(&arr[i], &arr[j]);
+			}
+		}
+		temp = arr[pivot];
+		arr[pivot] = arr[j];
+		arr[j] = temp;
+		quicksort(arr, first, j - 1);
+		quicksort(arr, j + 1, last);
+	}
+
+}
+void quicksortstr(char str[], int first, int last) {
+	int i, j, pivot;
+	char temp;
+	if (first < last) {
+		pivot = first;
+		i = first;
+		j = last;
+		while (i < j) {
+			while (str[i] <= str[pivot] && i < last)
+				i++;
+			while (str[j] > str[pivot])
+				j--;
+			if (i < j) {
+				temp = str[i];
+				str[i] = str[j];
+				str[j] = temp;
+			}
+
+		}
+		temp = str[pivot];
+		str[pivot] = str[j];
+		str[j] = temp;
+		quicksortstr(str, first, j - 1);
+		quicksortstr(str, j + 1, last);
+	}
+}
+
+
+void heapify(int arr[], int n, int i) {
+	int largest = i;
+	int left = 2 * i + 1;
+	int right = 2 * i + 2;
+
+	if (left < n && arr[left] > arr[largest])
+		largest = left;
+
+	if (right < n && arr[right] > arr[largest])
+		largest = right;
+	if (largest != i) {
+		swap(&arr[i], &arr[largest]);
+		heapify(arr, n, largest);
+	}
+}
+void heapsort(int arr[], int size) {
+
+	for (int i = size / 2 - 1; i >= 0; i--)
+		heapify(arr, size, i);
+
+
+	for (int i = size - 1; i >= 0; i--) {
+		swap(&arr[0], &arr[i]);
+		heapify(arr, i, 0);
+	}
+}
+void heapifystr(char str[], int n, int i) {
+	char temp;
+	int largest = i;
+	int left = 2 * i + 1;
+	int right = 2 * i + 2;
+	if (left<n && str[left]>str[largest])
+		largest = left;
+	if (right<n && str[right]>str[largest])
+		largest = right;
+	if (largest != i) {
+		temp = str[i];
+		str[i] = str[largest];
+		str[largest] = temp;
+		heapifystr(str, n, largest);
+	}
+
+}
+void heapstr(char str[], int size) {
+	char temp;
+	for (int i = size / 2 - 1; i >= 0; i--)
+		heapifystr(str, size, i);
+
+	for (int i = size - 1; i >= 0; i--) {
+		temp = str[0];
+		str[0] = str[i];
+		str[i] = temp;
+		heapifystr(str, i, 0);
+	}
+
+
+
+
+}
+
 void bubblesort(int arr[], int size) {
 
 
@@ -334,6 +453,42 @@ int main() {
 	}
 	end_t = clock();
 	printf("     sort time: %f\n", ((double)end_t - start_t) / CLOCKS_PER_SEC);
+start_t = clock(); //quicksort for number 
+	quicksort(arr, 0, 9);
+	printf("your array sorted with quicksort:");
+	for (int i = 0; i < 10; i++) {
+		printf("%d", arr[i]);
+	}
+	end_t = clock();
+	printf("     sort time: %f\n", ((double)end_t - start_t) / CLOCKS_PER_SEC);
+	start_t = clock();//quicksort for name
+	quicksortstr(str, 0, size2);
+	printf("your  string sorted with quicksort:");
+	for (int i = 0; i < 10; i++) {
+		printf("%c", str[i]);
+	}
+	end_t = clock();
+	printf("     sort time: %f\n", ((double)end_t - start_t) / CLOCKS_PER_SEC);
+	start_t = clock();//heapsort for number 
+	heapsort(arr, 10);
+	printf("your array sorted with heapsort:");
+	for (int i = 0; i < 10; i++) {
+		printf("%d", arr[i]);
+
+	}
+	end_t = clock();
+	printf("     sort time: %f\n", ((double)end_t - start_t) / CLOCKS_PER_SEC);
+	start_t = clock();//heapsort for name
+	heapstr(str, size2);
+	printf("your  string sorted with heapsort:");
+
+	for (int i = 0; i < 10; i++) {
+		printf("%c", str[i]);
+	}
+	end_t = clock();
+	printf("     sort time: %f\n", ((double)end_t - start_t) / CLOCKS_PER_SEC);
+
+
 
 
 
